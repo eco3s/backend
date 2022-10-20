3cd4f645eef2ac37ea2e742d04084a4cb8eb4ff6
Abiria <abiria.dev@gmail.com>
2022-10-20 14:52:46 +0900

    test(e2e): added e2e and unit tests for users

--- src/controllers/users/users.controller.spec.ts
+++ src/controllers/users/users.controller.spec.ts
@@ -1,18 +1,30 @@
-import { Test, TestingModule } from '@nestjs/testing';
-import { UsersController } from './users.controller';
+import { Test, TestingModule } from '@nestjs/testing'
+import { PrismaService } from '../../prisma/prisma.service'
+import { UsersService } from '../../models/users/users.service'
+import { UsersController } from './users.controller'
+import { DeepMockProxy, mockDeep } from 'jest-mock-extended'
+import { PrismaClient } from '@prisma/client'
 
 describe('UsersController', () => {
-  let controller: UsersController;
+	let controller: UsersController
+	let prisma: DeepMockProxy<PrismaClient>
 
-  beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      controllers: [UsersController],
-    }).compile();
+	beforeEach(async () => {
+		const module: TestingModule =
+			await Test.createTestingModule({
+				controllers: [UsersController],
+				providers: [UsersService, PrismaService],
+			})
+				.overrideProvider(PrismaService)
+				.useValue(mockDeep<PrismaClient>())
+				.compile()
 
-    controller = module.get<UsersController>(UsersController);
-  });
+		controller =
+			module.get<UsersController>(UsersController)
+		prisma = module.get(PrismaService)
+	})
 
-  it('should be defined', () => {
-    expect(controller).toBeDefined();
-  });
-});
+	it('should be defined', () => {
+		expect(controller).toBeDefined()
+	})
+})
--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,12 +1,16 @@
 import { Test, TestingModule } from '@nestjs/testing'
 import { INestApplication } from '@nestjs/common'
-import * as request from 'supertest'
-import { AppModule } from './../src/app.module'
+import request from 'supertest'
+import { AppModule } from '../src/app.module'
+import { PrismaService } from '../src/prisma/prisma.service'
 
-describe('AppController (e2e)', () => {
+describe('CRUD users', () => {
 	let app: INestApplication
+	let server: any
 
-	beforeEach(async () => {
+	jest.setTimeout(10000)
+
+	beforeAll(async () => {
 		const moduleFixture: TestingModule =
 			await Test.createTestingModule({
 				imports: [AppModule],
@@ -14,12 +18,78 @@ describe('AppController (e2e)', () => {
 
 		app = moduleFixture.createNestApplication()
 		await app.init()
+		server = app.getHttpServer()
+
+		const pr = app.get(PrismaService)
+		await pr.user.deleteMany()
 	})
 
-	it('/ (GET)', () => {
-		return request(app.getHttpServer())
-			.get('/')
-			.expect(200)
-			.expect('Hello World!')
+	const user = {
+		name: 'Abiria',
+		id: null,
+	}
+
+	it('must return empty array', async () => {
+		const res = await request(server).get('/users')
+
+		expect(res.body).toHaveLength(0)
+	})
+
+	it('must create new user', async () => {
+		const res = await request(server)
+			.post('/users')
+			.send(user)
+
+		expect(res.status).toBe(201)
+		expect(res.body.name).toBe(user.name)
+		expect(typeof res.body.id).toBe('string')
+
+		user.id = res.body.id
+	})
+
+	it('must return one user', async () => {
+		const res = await request(server).get('/users')
+
+		expect(res.body).toHaveLength(1)
+	})
+
+	// it('must return newly created user', async () => {
+	// 	const res = await request(server).get(
+	// 		`/users/${user.id}`,
+	// 	)
+	//
+	// 	expect(res.body.name).toBe(user.name)
+	// })
+	//
+	// it("must change user's name", async () => {
+	// 	const newName = 'newName'
+	//
+	// 	const res = await request(server)
+	// 		.patch(`/users/${user.id}`)
+	// 		.send({
+	// 			name: newName,
+	// 		})
+	//
+	// 	expect(res.body.name).toBe(newName)
+	//
+	// 	user.name = newName
+	// })
+	//
+	// it('must delete user', async () => {
+	// 	const res = await request(server).delete(
+	// 		`/users/${user.id}`,
+	// 	)
+	//
+	// 	expect(res.body.name).toBe(user.name)
+	// })
+	//
+	// it('must return empty array again', async () => {
+	// 	const res = await request(server).get('/users')
+	//
+	// 	expect(res.body).toHaveLength(0)
+	// })
+
+	afterAll(async () => {
+		await app.close()
 	})
 })
